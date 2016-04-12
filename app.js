507ad68bc61765aff660230118ad58a677335ddf
yulia3 <yulia@alkemy.io>
2016-04-11 18:31:31 -0600

    angular form

---
+++ app.js
@@ -0,0 +1,14 @@
+// var app = angular.module('app', []);
+
+// app.controller('MainCtrl', function($scope) {  
+//   $scope.save = function(model) {
+//     $scope.captureLoginUserForm.submitted = true;
+    
+//     if($scope.captureLoginUserForm.$valid) {
+//       // submit to db
+//       console.log(model); 
+//     } else {
+//       console.log('Errors in form data');
+//     }
+//   };
+// });
